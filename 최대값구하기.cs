28e2232a0b62600093d5b35d78f6c711c7dd2fc0
HWOwoo <101438486+HWOwoo@users.noreply.github.com>
2022-11-25 11:27:24 +0900

    22.11.25

---
+++ 최대값구하기.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C_manage
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 0;
+            int b = 0;
+            int max = 0;
+
+            for ( int i = 0; i < 9; i++)
+            {
+                string[] s = Console.ReadLine().Split(); // 3, 23, 85, 34, 17 ,74 ,25 ,52 ,65
+
+                for ( int j = 0; j < 9; j++) {
+                    if (max < int.Parse(s[j]))
+                    {
+                        max = int.Parse(s[j]);
+                        a = i + 1; 
+                        b = j + 1;
+                    }
+                }
+            }
+            if ( max == 0 )
+            {
+                a = 1;
+                b = 1;
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
